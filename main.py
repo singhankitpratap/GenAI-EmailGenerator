668507dd654334e315bbefec11eb389bc5ce303d
singhankitpratap <78484221+singhankitpratap@users.noreply.github.com>
2024-12-14 14:33:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -34,4 +34,4 @@ if __name__ == "__main__":
     workflow_model = Workflow()
     portfolio_manager = PortfolioManager()
     st.set_page_config(layout="wide", page_title="Email Generator")
-    generate_cold_email_app(workflow_model, portfolio_manager, clean_text_content)
+    generate_email_app(workflow_model, portfolio_manager, clean_text_content)
